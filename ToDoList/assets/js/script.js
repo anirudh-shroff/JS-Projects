5cccaf4198c7b40df6ec89262d999d8bd4b176f2
anirudh-shroff <shroffstar8@gmail.com>
2025-10-31 12:12:53 +0530

    base code complete

--- ToDoList/assets/js/script.js
+++ ToDoList/assets/js/script.js
@@ -1,12 +1,21 @@
 let taskList = [];
 
+function getPriority(id){
+    switch(parseInt(id)){
+        case 1 :
+            return  "<span>high</span>";
+        case 2 :
+            return  "<span>medium</span>";
+        case 3 :
+            return  "<span>low</span>";
+    }
+}
+
 $("#add").on("click",()=>{
     // console.log("hi")
     let task = document.getElementById("taskInp").value;
     let priority = document.getElementById("priority").value;
     // console.log(task, priority);
-
-    let date = new Date();
 
     let taskObj = {
         id : Date.now(), 
@@ -15,5 +24,13 @@ $("#add").on("click",()=>{
     // console.log(taskObj);
 
     taskList.push(taskObj);
-    console.log(taskList)
+    // console.log(taskList)
+  
+    document.getElementById("Tasks").innerHTML = "";
+  
+    taskList.forEach((data, idx) => {
+        document.getElementById("Tasks").innerHTML = `
+        <h1>${idx + 1} ${data.task} ${getPriority(data.priority)}</h1>
+        `
+    });
 })
