a1840ac4658b5b269bd7756e9b1e87699607e61f
anirudh-shroff <shroffstar8@gmail.com>
2025-11-19 12:17:32 +0530

    to-do lists

--- ToDoList/assets/js/script.js
+++ ToDoList/assets/js/script.js
@@ -1,36 +1,107 @@
-let taskList = [];
+let tasks = [];
+let currentFilter = "all";
+let taskList = document.getElementById("taskList");
+let empty = document.getElementById("empty");
+function setPriority(task) {
+    let priority = task.taskPriority;
+    switch (priority) {
+        case "high":
+            {
+                return 0;
+            }
+        case "medium":
+            {
+                return 1;
+            }
+        case "low":
+            {
+                return 2;
+            }
+        default:
+            {
+                return -1;
+            }
 
-function getPriority(id){
-    switch(parseInt(id)){
-        case 1 :
-            return  "<span>high</span>";
-        case 2 :
-            return  "<span>medium</span>";
-        case 3 :
-            return  "<span>low</span>";
     }
 }
 
-$("#add").on("click",()=>{
-    // console.log("hi")
-    let task = document.getElementById("taskInp").value;
-    let priority = document.getElementById("priority").value;
-    // console.log(task, priority);
+function clearInp() {
+    document.getElementById("taskHead").value = "";
+    document.getElementById("taskDsc").value = ""
+    document.getElementById("prioritySelect").value = ""
+}
 
-    let taskObj = {
-        id : Date.now(), 
-        task, 
-        priority}
-    // console.log(taskObj);
+function deleteTask(id) {
+    let idx = tasks.findIndex(t => t.taskID === id);
+    if (idx === -1) {
+        window.alert("No tasks found!");
+        return;
+    }
+    tasks.splice(idx, 1);
+    taskRender();
+}
 
-    taskList.push(taskObj);
-    // console.log(taskList)
-  
-    document.getElementById("Tasks").innerHTML = "";
-  
-    taskList.forEach((data, idx) => {
-        document.getElementById("Tasks").innerHTML = `
-        <h1>${idx + 1} ${data.task} ${getPriority(data.priority)}</h1>
+function taskRender() {
+    let filteredTasks = tasks;
+
+    if (currentFilter !== "all") {
+        filteredTasks = tasks.filter(t => t.taskPriority === currentFilter);
+    }
+
+    if (!tasks.length) {
+        empty.innerHTML = `
+            <div class="empty">
+                <h3 class="text-center">What's on your mind?</h3>
+                <p class="text-center">Let's go get some work done</p>
+            </div>
         `
-    });
+        taskList.innerHTML = "";
+        return;
+    }
+    empty.innerHTML = "";
+    taskList.innerHTML = filteredTasks.map
+        (t => `<div class="card priority-${t.taskPriority} m-2 w-25">
+        <div class="card-header">
+            ${t.taskPriority}
+        </div>
+            <div class="card-body">
+                <h5 class="card-title">${t.taskHead}</h5>
+                <p class="card-text">${t.taskDsc}</p>
+                <button href="#" class="btn p-0 priority-${t.taskPriority}-bg" onclick="deleteTask(${t.taskID})">
+                    <i class="ri-close-circle-fill"></i>
+                </button>
+            </div>
+        </div>`).join("");
+    clearInp();
+}
+
+document.getElementById("addBtn").addEventListener("click", () => {
+    let taskHead = document.getElementById("taskHead").value;
+    let taskDsc = document.getElementById("taskDsc").value;
+    let taskPriority = document.getElementById("prioritySelect").value;
+
+    if (!taskHead || !taskDsc || !taskPriority) {
+        window.alert("Pleas fill the boxes first");
+        return;
+    } else {
+        let task = {
+            taskHead,
+            taskDsc,
+            taskID: Date.now(),
+            taskPriority,
+        }
+
+        tasks.push(task);
+        // console.log(tasks.map(t => `${t.taskDsc} - ${t.taskHead} - ID : ${t.taskID} taskPriority : ${t.taskPriority}`))
+        tasks.sort((a, b) => setPriority(a) - setPriority(b));
+        taskRender();
+    }
+
 })
+
+function changeTaskPriority(e) {
+    currentFilter = e.target.value;
+    taskRender();
+}
+
+document.getElementById("filter").addEventListener("change", changeTaskPriority)
